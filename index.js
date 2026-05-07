e12200002296a04712330aef930b9035c4ab1f9c
Draven-maker15 <draven.davenport@students.msswarriors.org>
2026-05-07 13:59:12 -0500

    Fix key name in school object for average class size

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const school = {
   name: "Your School Name",
-  'average_class_size': 25,
+  'average class size': 25,
   mascot: "Warriors" ,
 };
-document.getElementById("result").innerHTML = `${school.name} has an average class size of ${school["average_class_size"]} and their mascot is a ${school.mascot}`;
+document.getElementById("result").innerHTML = `${school.name} has an average class size of ${school["average class size"]} and their mascot is a ${school.mascot}`;
