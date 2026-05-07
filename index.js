ef9b2fb9c40c8059688f0381b81eaec8badf23ef
Draven-maker15 <draven.davenport@students.msswarriors.org>
2026-05-07 13:34:38 -0500

    Add school object and display information on page

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+const school = {
+  name: "Your School Name",
+  average_class-size: 25,
+  mascot: "Warriors"
+};
+document.getElementById("result").innerHTML = `${school.name} has an average class size of ${school.average_class_size} and their mascot is a ${school.mascot}`;
